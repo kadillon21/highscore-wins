90eb16fb1acd9cbcd663cd0f7623ffe4d3c10697
kadillon21 <kadillon21@gmail.com>
2026-04-15 11:29:49 -0500

    Exercise 3 complete

--- src/main/java/com/pluralsight/HighscoreApp.java
+++ src/main/java/com/pluralsight/HighscoreApp.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class HighscoreApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter a game score: ");
+        String userInput = scanner.nextLine();
+
+        String[] splitOne = userInput.split("\\|");
+        String[] teams = splitOne[0].split(":");
+        String[] scores = splitOne[1].split(":");
+
+        if(Integer.parseInt(scores[0]) > Integer.parseInt(scores[1])){
+            System.out.println("Winner: " + teams[0]);
+        } else {
+            System.out.println("Winner: " + teams[1]);
+        }
+
+    }
 }
